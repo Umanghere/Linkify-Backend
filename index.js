8a013795cae32ba8cce48a31105b5131a0f9de5b
umanghere <umangbansal1st@gmail.com>
2025-04-27 10:03:03 +0530

    Backend for Linkify Application

---
+++ index.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const apiRoutes = require("./routes/apiRoutes");
+const connection = require("./connection");
+const { getURLHandler } = require("./controllers/url");
+const cron = require("node-cron");
+const URL = require("./models/url");
+
+require("dotenv").config();
+connection();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+/* Middlewares */
+app.use(cors());
+app.use(express.json());
+
+/* Routes */
+app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
+
+/* Cron job to deactivate expired URLs */
+async function deactivateUrls() {
+  try {
+    const now = new Date();
+    const result = await URL.updateMany(
+      { "urls.isActive": true, "urls.expiresAt": { $lt: now } },
+      { $set: { "urls.$[].isActive": false } }
+    );
+    console.log(`${result.modifiedCount} user(s) had expired URLs deactivated.`);
+  } catch (error) {
+    console.error("Error deactivating expired URLs:", error.message);
+  }
+}
+
+cron.schedule("* * * * *", () => {
+  console.log("Running expiration check for URLs...");
+  deactivateUrls();
+});
+
+/* Short URL redirect route */
+app.get("/:id", getURLHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Frontend is served at http://localhost:${PORT}`);
+});
