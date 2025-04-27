f3487d87fb1b78746ee48d3246056c191ddbae5e
umanghere <umangbansal1st@gmail.com>
2025-04-27 10:20:05 +0530

    Updated CORS and Global Host

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 /* Middlewares */
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 /* Routes */
