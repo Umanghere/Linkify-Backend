9a21f9aefa17e3d551e89ff7518d0fcf8da0ca28
umanghere <umangbansal1st@gmail.com>
2025-04-27 11:25:57 +0530

    Updated package.json file

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const URL = require("./models/url");
 require("dotenv").config();
 connection();
 
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 /* Middlewares */
 app.use(cors({
-  origin: "http://localhost:5173", // or frontend deployed URL later
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
@@ -29,8 +30,8 @@ async function deactivateUrls() {
     const now = new Date();
     const result = await URL.updateMany(
       { "urls.isActive": true, "urls.expiresAt": { $lt: now } },
-      { $set: { "urls.$[].isActive": false }
-    });
+      { $set: { "urls.$[].isActive": false } }
+    );
     console.log(`${result.modifiedCount} user(s) had expired URLs deactivated.`);
   } catch (error) {
     console.error("Error deactivating expired URLs:", error.message);
@@ -45,4 +46,7 @@ cron.schedule("* * * * *", () => {
 /* Short URL redirect route */
 app.get("/:id", getURLHandler);
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Frontend is served at http://localhost:${PORT}`);
+});
