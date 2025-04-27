0999b010c45d5fe8c979b3ed25be8bf036953ea2
umanghere <umangbansal1st@gmail.com>
2025-04-27 22:47:17 +0530

    Updated CORS

--- index.js
+++ index.js
@@ -14,8 +14,19 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 /* Middlewares */
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://linkify-frontend-sooty.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
 app.use(express.json());
